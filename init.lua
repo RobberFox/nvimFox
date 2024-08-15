a177924d910e96bca8c517a5a6fc840a97bcface
Robert Hayrapetyan <robchikbob@gmail.com>
2024-08-15 15:22:48 +0400

    Changed yank highlight speed

--- init.lua
+++ init.lua
@@ -73,7 +73,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank { timeout = 80 }
+    vim.highlight.on_yank { timeout = 60 }
   end,
 })
 
