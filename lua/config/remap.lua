6607b63345fc4335b921ca3b51cdbb0bf0ea9067
Robert Hayrapetyan <robchikbob@gmail.com>
2024-09-26 16:05:05 +0400

    Added the "increment alphabetically" toggle remap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -19,8 +19,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Sigma yank-substitute remap
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set('n', '<Esc>', vim.cmd.nohlsearch)
+vim.keymap.set("n", "<Esc>", vim.cmd.nohlsearch)
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "<S-CR>", "<nop>")
 
 -- See: `:help quickfix`
 vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>zz")
@@ -28,14 +29,27 @@ vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
-vim.keymap.set('n', '<F2>', '<cmd>split term://bash<enter>')
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>') -- <Esc><Esc> won't work on all terminal emulators
-vim.keymap.set('n', '<F7>', '<cmd>!gcc % && ./a.out <CR>')
+vim.keymap.set("n", "<F2>", "<cmd>split term://bash<enter>")
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>") -- <Esc><Esc> won"t work on all terminal emulators
+vim.keymap.set("n", "<F7>", "<cmd>!gcc % && ./a.out <CR>")
 
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' }) -- Diagnostic keymap
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" }) -- Diagnostic keymap
 
 -- See: `:help wincmd` - list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
+-- NOTE: ### MEGA KEYBINDING ###
+require("config.helper_function") -- table printing function
+
+vim.keymap.set("n", "<leader>z", function()
+	if (vim.opt.nrformats:get()[3] == "alpha") then
+		vim.opt.nrformats:remove{"alpha"}
+		vim.notify(tprint(vim.opt.nrformats:get()))
+	else
+		vim.opt.nrformats:append{"alpha"}
+		vim.notify(tprint(vim.opt.nrformats:get()))
+	end
+end) -- Increment character alphabetically
