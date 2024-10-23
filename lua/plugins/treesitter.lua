2d520e46dbba707a44584124f2db259784cf2980
Robert Hayrapetyan <robchikbob@gmail.com>
2024-10-23 23:26:24 +0400

    Changed treesitter for large files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,17 @@ return {
 		opts = {
 			ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
 			auto_install = true,
-			highlight = { enable = true },
+			highlight = {
+				enable = true,
+
+				disable = function(lang, buf)
+					local max_filesize = 100 * 1024 -- 100 KB
+					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					if ok and stats and stats.size > max_filesize then
+						return true
+					end
+				end,
+			},
 		},
 		config = function(_, opts)
 			require('nvim-treesitter.configs').setup(opts)
